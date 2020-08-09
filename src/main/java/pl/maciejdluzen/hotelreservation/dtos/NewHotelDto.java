0d74109b3dc58a46fc47e8e1db87906f23709256
unknown <maciej.dluzen@yahoo.com>
2020-08-09 15:23:16 +0200

    Created NewHotelDto

---
+++ src/main/java/pl/maciejdluzen/hotelreservation/dtos/NewHotelDto.java
@@ -0,0 +1,46 @@
+package pl.maciejdluzen.hotelreservation.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewHotelDto {
+
+    @NotNull
+    @NotBlank
+    private String name;
+    @NotNull
+    @NotBlank
+    private String street;
+    @NotNull
+    @NotBlank
+    private String number;
+    @NotNull
+    @NotBlank
+    private String city;
+    @NotNull
+    @NotBlank
+    private String postCode;
+    @NotNull
+    @NotBlank
+    private String phoneNumber;
+    @NotNull
+    @NotBlank
+    @Email
+    private String emailAddress;
+
+
+
+
+
+
+}
