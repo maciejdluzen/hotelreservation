a1d025ddeef53cd8ad6033b75d25111c2b7517b2
unknown <maciej.dluzen@yahoo.com>
2020-08-09 14:30:54 +0200

    Created room selection page front-end

--- src/main/java/pl/maciejdluzen/hotelreservation/controllers/HelloWorldController.java
+++ src/main/java/pl/maciejdluzen/hotelreservation/controllers/HelloWorldController.java
@@ -1,14 +1,15 @@
 package pl.maciejdluzen.hotelreservation.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
     @GetMapping("/hello")
     public String helloWorld() {
 
-        return "Hello Maciej";
+        return "reservation/roomselection";
     }
 }
--- src/main/java/pl/maciejdluzen/hotelreservation/services/impl/DefaultValidationService.java
+++ src/main/java/pl/maciejdluzen/hotelreservation/services/impl/DefaultValidationService.java
@@ -1,18 +1,21 @@
 package pl.maciejdluzen.hotelreservation.services.impl;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.maciejdluzen.hotelreservation.domain.repositories.UserRepository;
 import pl.maciejdluzen.hotelreservation.services.ValidationService;
 
 @Service
 @Slf4j
-@AllArgsConstructor
 public class DefaultValidationService implements ValidationService {
 
     private final UserRepository userRepository;
+
+    public DefaultValidationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public boolean isUniqueEmailAddress(String emailAddress) {
