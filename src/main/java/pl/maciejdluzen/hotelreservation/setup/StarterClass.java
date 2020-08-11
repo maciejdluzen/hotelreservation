5fd52fc23c12b41fa5ba4dc23bec8b1c62fd79b7
Maciej Dluzen <maciej.dluzen@capgemini.com>
2020-08-11 12:27:46 +0200

    Added hotel to the StarterClass

--- src/main/java/pl/maciejdluzen/hotelreservation/setup/StarterClass.java
+++ src/main/java/pl/maciejdluzen/hotelreservation/setup/StarterClass.java
@@ -6,7 +6,9 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import pl.maciejdluzen.hotelreservation.domain.entities.Hotel;
 import pl.maciejdluzen.hotelreservation.domain.entities.Role;
+import pl.maciejdluzen.hotelreservation.domain.repositories.HotelRepository;
 import pl.maciejdluzen.hotelreservation.domain.repositories.RoleRepository;
 
 @Component
@@ -15,9 +17,11 @@ public class StarterClass implements ApplicationRunner {
     private Logger LOG = LoggerFactory.getLogger(getClass());
 
     private final RoleRepository roleRepository;
+    private final HotelRepository hotelRepository;
 
-    public StarterClass(RoleRepository roleRepository) {
+    public StarterClass(RoleRepository roleRepository, HotelRepository hotelRepository) {
         this.roleRepository = roleRepository;
+        this.hotelRepository = hotelRepository;
     }
 
     @Override
@@ -36,6 +40,16 @@ public class StarterClass implements ApplicationRunner {
         Role roleGuest = new Role();
         roleGuest.setId(3L);
         roleGuest.setName("ROLE_GUEST");
-        roleRepository.save(roleGuest);   
+        roleRepository.save(roleGuest);
+
+        Hotel hotel1 = new Hotel();
+        hotel1.setName("Plaza Inn Wrocław - Bielany");
+        hotel1.setStreet("Karkonoska");
+        hotel1.setNumber("124");
+        hotel1.setCity("Wrocław");
+        hotel1.setPostCode("53-522");
+        hotel1.setPhoneNumber("(+48) 71 342-44-78");
+        hotel1.setEmailAddress("wroclawbielany@plazainn.pl");
+        hotelRepository.save(hotel1);
     }
 }
