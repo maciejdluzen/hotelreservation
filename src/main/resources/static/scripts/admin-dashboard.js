f07683b395506c280e47bb64aeea15be07f5c3af
unknown <maciej.dluzen@yahoo.com>
2020-08-11 23:20:13 +0200

    Created New Room form

---
+++ src/main/resources/static/scripts/admin-dashboard.js
@@ -0,0 +1,16 @@
+let messageField = document.getElementById('messages');
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+if(urlParams.get('msg') === 'addedHotel') {
+    messageField.firstElementChild.classList.add('alert');
+    messageField.firstElementChild.classList += ' alert-success'
+    messageField.firstElementChild.innerHTML += '<p>Dodano nowy hotel</p>'
+    messageField.firstElementChild.classList += ' animate_animated'
+    messageField.firstElementChild.classList += ' animate_fadeOut'
+}
+
+$("#showFormBtn").click(function(){
+    $("#newHotelForm").toggle();
+});
